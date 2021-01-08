6dd895900d775540742eda02084f72bf88284d12
zjz1988314 <zjz1988314@163.com>
2021-01-08 07:50:07 +0000

    Log: add log level for precise control.

--- libutils/include/Log.h
+++ libutils/include/Log.h
@@ -28,6 +28,23 @@ extern "C" {
 #define LOG_TAG "MP_TAG"
 #endif
 
+#ifndef LOG_LEVEL
+#define LOG_LEVEL 4
+#endif
+
+/*
+ * Android log priority values, in ascending priority order.
+ */
+#define MERCURY_LOG_UNKNOWN 0
+#define MERCURY_LOG_DEFAULT 1
+#define MERCURY_LOG_VERBOSE 2
+#define MERCURY_LOG_DEBUG   3
+#define MERCURY_LOG_INFO    4
+#define MERCURY_LOG_WARN    5
+#define MERCURY_LOG_ERROR   6
+#define MERCURY_LOG_FATAL   7
+#define MERCURY_LOG_SILENT  8
+
 #define CONDITION(cond)     (__builtin_expect(!!(cond), 1))
 
 static char *timeString() {
@@ -52,11 +69,19 @@ static char *timeString() {
 
 #if MP_LOG_DEBUG
 
+#if (LOG_LEVEL<=MERCURY_LOG_VERBOSE)
 #define ALOGV(format, ...) \
-printf("%s %d %d V %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+    printf("%s %d %d V %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define ALOGV(format, ...)
+#endif
 
+#if (LOG_LEVEL<=MERCURY_LOG_DEBUG)
 #define ALOGD(format, ...) \
-printf("%s %d %d D %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+    printf("%s %d %d D %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define ALOGD(format, ...)
+#endif
 
 
 #else
@@ -69,14 +94,26 @@ printf("%s %d %d D %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)s
 
 //-----------------------------------------------------------------------------------------------
 
+#if (LOG_LEVEL<=MERCURY_LOG_INFO)
 #define ALOGI(format, ...) \
-printf("%s %d %d I %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+    printf("%s %d %d I %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define ALOGI(format, ...)
+#endif
 
+#if (LOG_LEVEL<=MERCURY_LOG_WARN)
 #define ALOGW(format, ...) \
-printf("%s %d %d W %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+    printf("%s %d %d W %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define ALOGW(format, ...)
+#endif
 
+#if (LOG_LEVEL<=MERCURY_LOG_ERROR)
 #define ALOGE(format, ...) \
-printf("%s %d %d E %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+    printf("%s %d %d E %s: [%s:%d] " format"\n", timeString(), (int)getpid(), (int)syscall(SYS_gettid), LOG_TAG, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define ALOGE(format, ...)
+#endif
 
 
 #define LOG_ALWAYS_FATAL(...) do { ALOGE(__VA_ARGS__); assert(0); } while(0)
